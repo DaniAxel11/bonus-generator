34675200095b160a3ddd978227b2ea7ad51f94f6
Axel Gonzalez <dagonzalezm@truper.com>
2026-04-15 12:57:34 -0600

    se agregan cambios al pom para que detencte mapstruct y lombook

--- src/main/java/com/truper/bonusgenerator/model/dto/mapper/CommitMapper.java
+++ src/main/java/com/truper/bonusgenerator/model/dto/mapper/CommitMapper.java
@@ -6,7 +6,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
 
-@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface CommitMapper {
     Commit toEntity(CommitDto commitDto);
 
